f81e03452362e6d071dabe8a9825205e16e20a80
Tony George <tonygk.4u@gmail.com>
2019-06-25 17:28:59 +0530

    Create cnn.py

---
+++ cnn.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 12:08:38 2019
+    
+@author: Tony.George
+"""
+
+import cv2 
+import os 
+import numpy as np 
+from random import shuffle 
+from tqdm import tqdm 
+
+import tflearn 
+from tflearn.layers.conv import conv_2d, max_pool_2d 
+from tflearn.layers.core import input_data, dropout, fully_connected 
+from tflearn.layers.estimator import regression 
+  
+import tensorflow as tf 
+import matplotlib.pyplot as plt
+
+TRAIN = 'C:/Users/tony.george/Documents/Kaggle/flexhose/train'
+TEST =  'C:/Users/tony.george/Documents/Kaggle/flexhose/test'
+
+IMG_SIZE = 50
+LR = 1e-3
+
+'''Setting up the model which will help with tensorflow models'''
+MODEL_NAME = 'flexihose-{}-{}.model'.format(LR, '6conv-basic') 
+
+def label_img(word_label):
+  
+ # DIY One hot encoder
+    if word_label == 'good': return [1, 0, 0]
+    elif word_label == 'frey': return [0, 1, 0]
+    elif word_label == 'rust': return [0, 0, 1]
+
+
+training_data1 = [] 
+
+for dir in os.listdir(TRAIN):
+    path = os.path.join(TRAIN, dir)
+    for img in tqdm(os.listdir(path)): 
+        
+        # labeling the images 
+        label = label_img(dir) 
+      
+        path1 = os.path.join(path, img) 
+      
+        # loading the image from the path and then converting them into 
+        # greyscale for easier covnet prob 
+        img = cv2.imread(path1, cv2.IMREAD_GRAYSCALE) 
+      
+        # resizing the image for processing them in the covnet 
+        img = cv2.resize(img, (IMG_SIZE, IMG_SIZE)) 
+        training_data1.append([np.array(img), np.array(label)]) 
+
+testing_data1 = [] 
+for img in tqdm(os.listdir(TEST)): 
+    path = os.path.join(TEST, img) 
+    img_num = img.split('.')[0] 
+    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE) 
+    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE)) 
+    testing_data1.append([np.array(img), img_num]) 
+    
+shuffle(testing_data1) 
+np.save('test_data1.npy', testing_data1) 
+
+tf.reset_default_graph() 
+convnet = input_data(shape =[None, IMG_SIZE, IMG_SIZE, 1], name ='input') 
+  
+convnet = conv_2d(convnet, 32, 5, activation ='relu') 
+convnet = max_pool_2d(convnet, 5) 
+  
+convnet = conv_2d(convnet, 64, 5, activation ='relu') 
+convnet = max_pool_2d(convnet, 5) 
+  
+convnet = conv_2d(convnet, 128, 5, activation ='relu') 
+convnet = max_pool_2d(convnet, 5) 
+  
+convnet = conv_2d(convnet, 64, 5, activation ='relu') 
+convnet = max_pool_2d(convnet, 5) 
+  
+convnet = conv_2d(convnet, 32, 5, activation ='relu') 
+convnet = max_pool_2d(convnet, 5) 
+  
+convnet = fully_connected(convnet, 1024, activation ='relu') 
+convnet = dropout(convnet, 0.8) 
+  
+convnet = fully_connected(convnet, 2, activation ='softmax') 
+convnet = regression(convnet, optimizer ='adam', learning_rate = LR, 
+      loss ='categorical_crossentropy', name ='targets') 
+  
+model = tflearn.DNN(convnet, tensorboard_dir ='log') 
+
+X1 = np.array([i[0] for i in training_data1]).reshape(-1, IMG_SIZE, IMG_SIZE, 1) 
+Y1 = [i[1] for i in training_data1] 
+#test_x = np.array([i[0] for i in test]).reshape(-1, IMG_SIZE, IMG_SIZE, 1) 
+#test_y = [i[1] for i in test] 
+
+model.fit({'input': X1}, {'targets': Y1}, n_epoch = 5,  
+    #validation_set =({'input': test_x}, {'targets': test_y}),  
+    snapshot_step = 500, show_metric = True, run_id = MODEL_NAME) 
+model.save(MODEL_NAME) 
